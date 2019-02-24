a0c65e28861a13af9ab28bcadd2713a6cb0c65ec
DmitriyS87 <schukindmitriy@gmail.com>
2019-02-25 00:20:27 +0800

    remakes filter component

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,26 @@
-export default ({textFilter, condition = ``}) => {
-  const filter = `<input type="radio" id="filter-${textFilter.toLowerCase()}" name="filter" value="${textFilter.toLowerCase()}" ${condition}>
+const makeFliterHTML = (textFilter, condition) => {
+  return `<input type="radio" id="filter-${textFilter.toLowerCase()}" name="filter" value="${textFilter.toLowerCase()}" ${condition}>
   <label class="trip-filter__item" for="filter-${textFilter.toLowerCase()}">${textFilter}</label>`;
+};
+
+const makeFilter = ({textFilter, condition = ``}, onClick = () => {}) => {
+  const template = document.createElement(`template`);
+  const filterHTML = makeFliterHTML(textFilter, condition);
+  template.innerHTML = filterHTML;
+  const filter = template.content;
+  filter.firstChild.addEventListener(`click`, () => {
+    onClick();
+  });
   return filter;
+};
+
+export default (filtersData, onClick) => {
+  const fragment = document.createDocumentFragment();
+  const arrayFilters = filtersData.map((filterData) => {
+    return makeFilter(filterData, onClick);
+  });
+  arrayFilters.forEach((filter) => {
+    fragment.appendChild(filter);
+  });
+  return fragment;
 };
