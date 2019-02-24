e6da33e5188aeac0a6878a99bd9115ccf5312391
DmitriyS87 <schukindmitriy@gmail.com>
2019-02-24 17:51:27 +0800

    adds trip-point to main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import makeFilter from './filter';
 import {render} from './utils';
 import {clearHtmlInside} from './utils';
+import makePointHtml from './trip-point';
 
 const FILTERS_DATA = [{
   textFilter: `Everything`,
@@ -13,7 +14,17 @@ const FILTERS_DATA = [{
   textFilter: `Past`
 }];
 
+const tripsData = {
+  icon: `✈️`,
+  title: `Flight to Geneva`,
+  timeTable: `10:00&nbsp;&mdash; 11:00`,
+  duration: `1h 30m`,
+  price: `&euro;&nbsp;20`,
+  offers: [`Upgrade to business +&euro;&nbsp;20`, `Select meal +&euro;&nbsp;20`]
+};
+
 const FILTER_FORM_CLASS = `.trip-filter`;
+const TRIP_DAY_CLASS = `.trip-day__items`;
 
 const filtersHtml = FILTERS_DATA.reduce((html, item) => {
   return ((html !== 0) ? html : ``) + makeFilter(item);
@@ -21,3 +32,6 @@ const filtersHtml = FILTERS_DATA.reduce((html, item) => {
 
 clearHtmlInside(FILTER_FORM_CLASS);
 render(filtersHtml, FILTER_FORM_CLASS);
+
+clearHtmlInside(TRIP_DAY_CLASS);
+render(makePointHtml(tripsData), TRIP_DAY_CLASS);
