a7c3f17562dd83b1dbf8cd140e3abfe5548f042e
DmitriyS87 <schukindmitriy@gmail.com>
2019-02-24 16:51:53 +0800

    makes trip-point module

---
+++ src/trip-point.js
@@ -0,0 +1,28 @@
+
+const offerLi = (offer) => {
+  return `<li>
+  <button class="trip-point__offer">${offer}</button>
+</li>`;
+};
+
+const renderOffers = (offers) => {
+  return offers.reduce((arr, item) => {
+    return offerLi(item);
+  }, 0);
+};
+
+export default ({icon = `🚕`, title = `Taxi to Airport`, timeTable = `10:00&nbsp;&mdash; 11:00`, duration = `1h 30m`, price = `&euro;&nbsp;20`, offers = `Order UBER +&euro;&nbsp;20`}) => {
+  return `
+  <article class="trip-point">
+    <i class="trip-icon">${icon}</i>
+    <h3 class="trip-point__title">${title}</h3>
+    <p class="trip-point__schedule">
+      <span class="trip-point__timetable">${timeTable}</span>
+      <span class="trip-point__duration">${duration}</span>
+    </p>
+    <p class="trip-point__price">${price}</p>
+      <ul class="trip-point__offers">
+        ${renderOffers(offers)}
+      </ul>
+  </article>`;
+};
