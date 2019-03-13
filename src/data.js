3b345deef8cab1a0833996081c010332f2f2b5a0
DmitriyS87 <schukindmitriy@gmail.com>
2019-03-13 20:19:13 +0800

    adds time and offers generation to data.js

--- src/data.js
+++ src/data.js
@@ -56,13 +56,21 @@ const OFFERS = [{
 const TEXT_DESCRIPTION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 
 const getRandomSentences = (text) => {
-  return text.split(`.`)[Math.round(Math.random() * 3)];
+  return text.split(`.`)[Math.round(Math.random() * 2)];
 };
 
 const getRandomOffers = (array) => {
-  return array.map((item)=>{
+  const offers = array.map((item)=>{
     return `${item.title} +${item.currency};&nbsp;${item.price}`;
-  })[Math.round(Math.random() * 2)];
+  });
+  const count = Math.round(Math.random() * 2);
+  const exportOffers = [];
+  if (count > 0) {
+    for (let i = 0; i < count; i++) {
+      exportOffers.push(offers[Math.floor(Math.random() * offers.length)]);
+    }
+  }
+  return exportOffers;
 };
 
 const timeOptions = {
@@ -71,19 +79,21 @@ const timeOptions = {
 };
 
 const getRandomTimeTable = () => {
-  const date = new Date();
-  const start = new Intl.DateTimeFormat(`en-US`, timeOptions).format(date.hour - Math.random(12 * Math.random()));
-  const end = new Intl.DateTimeFormat(`en-US`, timeOptions).format(date.hour + Math.random(12 * Math.random()));
+  const dateStart = Date.now() + Math.round(24 * Math.random()) * 60 * 60 * 1000;
+  const dateFinish = dateStart + Math.round(6 * Math.random()) * 60 * 60 * 1000;
+  const start = new Intl.DateTimeFormat(`en-US`, timeOptions).format(dateStart);
+  const end = new Intl.DateTimeFormat(`en-US`, timeOptions).format(dateFinish);
   return `${start}&nbsp;&mdash; ${end}`;
 };
 
 export default () => {
-  return {type: TYPES[Math.round(Math.random() * TYPES.length)],
-    city: CITIES[Math.round(Math.random() * CITIES.length)],
+  return {
+    type: TYPES[Math.floor(Math.random() * TYPES.length)],
+    city: CITIES[Math.floor(Math.random() * CITIES.length)],
     description: getRandomSentences(TEXT_DESCRIPTION),
     timeTable: getRandomTimeTable(),
-    duration: `1h 30m`,
-    price: `&euro;&nbsp;20`,
+    duration: `${Math.floor(Math.random() * 4)}h ${Math.floor(Math.random() * 12) * 5}m`,
+    price: `${DEFAULT_CURRENCY};&nbsp;${Math.floor(Math.random() * 10) * 5}`,
     offers: getRandomOffers(OFFERS),
     picture: `http://picsum.photos/300/150?r=${Math.random()}`
   };
