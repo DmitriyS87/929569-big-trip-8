6f80a7d3da910945408fde79f788f3ec4a06c5b5
DmitriyS87 <schukindmitriy@gmail.com>
2019-03-14 23:47:46 +0800

    fills get template()

--- src/trip-point-class.js
+++ src/trip-point-class.js
@@ -17,9 +17,30 @@ class TripPointClass {
     this._element = null;
   }
 
+  _renderOfferItem(offer) {
+    return `<li>
+    <button class="trip-point__offer">${offer}</button>
+  </li>`;
+  }
+
+  _renderOffersList(offers) {
+    return offers.map(this._renderOfferItem).join(``);
+  }
+
 
   get template() {
-
+    return `<article class="trip-point">
+    <i class="trip-icon">${this.type}</i>
+    <h3 class="trip-point__title">${this.type.type} to ${this.city}</h3>
+    <p class="trip-point__schedule">
+      <span class="trip-point__timetable">${this.timeTable}</span>
+      <span class="trip-point__duration">${this.duration}</span>
+    </p>
+    <p class="trip-point__price">${this.price}</p>
+      <ul class="trip-point__offers">
+        ${this._renderOffersList(this.offers)}
+      </ul>
+  </article>`;
   }
 
   render() {
