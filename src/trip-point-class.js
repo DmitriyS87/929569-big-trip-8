a8a6889a8ba2b9dee55d1b5cec902e70b6bd309d
DmitriyS87 <schukindmitriy@gmail.com>
2019-03-14 23:49:02 +0800

    marks class properties as a ptivate

--- src/trip-point-class.js
+++ src/trip-point-class.js
@@ -5,14 +5,14 @@ const createElement = (template) => {
 };
 class TripPointClass {
   constructor(data) {
-    this.type = data.type;
-    this.city = data.city;
-    this.price = data.price;
-    this.description = data.description;
-    this.timeTable = data.timeTable;
-    this.duration = data.duration;
-    this.offers = data.offers;
-    this.picture = data.picture;
+    this._type = data.type;
+    this._city = data.city;
+    this._price = data.price;
+    this._description = data.description;
+    this._timeTable = data.timeTable;
+    this._duration = data.duration;
+    this._offers = data.offers;
+    this._picture = data.picture;
 
     this._element = null;
   }
@@ -30,15 +30,15 @@ class TripPointClass {
 
   get template() {
     return `<article class="trip-point">
-    <i class="trip-icon">${this.type}</i>
-    <h3 class="trip-point__title">${this.type.type} to ${this.city}</h3>
+    <i class="trip-icon">${this._type}</i>
+    <h3 class="trip-point__title">${this._type.type} to ${this._city}</h3>
     <p class="trip-point__schedule">
-      <span class="trip-point__timetable">${this.timeTable}</span>
-      <span class="trip-point__duration">${this.duration}</span>
+      <span class="trip-point__timetable">${this._timeTable}</span>
+      <span class="trip-point__duration">${this._duration}</span>
     </p>
-    <p class="trip-point__price">${this.price}</p>
+    <p class="trip-point__price">${this._price}</p>
       <ul class="trip-point__offers">
-        ${this._renderOffersList(this.offers)}
+        ${this._renderOffersList(this._offers)}
       </ul>
   </article>`;
   }
