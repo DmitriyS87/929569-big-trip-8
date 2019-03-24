d9365798a009bb9156a8b8f8d4499cf02a72c3a6
DmitriyS87 <schukindmitriy@gmail.com>
2019-03-24 15:52:02 +0800

    adds base functions in Filter

--- src/filter.js
+++ src/filter.js
@@ -1,8 +1,8 @@
 import Component from "./component";
 
-const renderFilter = ({textFilter, condition = ``}, onClick = () => {}) => {
+const renderFilter = ({textFilter, condition = false}, onClick = () => {}) => {
   const template = document.createElement(`template`);
-  template.innerHTML = `<input type="radio" id="filter-${textFilter.toLowerCase()}" name="filter" value="${textFilter.toLowerCase()}" ${condition}>
+  template.innerHTML = `<input type="radio" id="filter-${textFilter.toLowerCase()}" name="filter" value="${textFilter.toLowerCase()}" ${condition ? `checked` : ``}>
   <label class="trip-filter__item" for="filter-${textFilter.toLowerCase()}">${textFilter}</label>`;
   const filter = template.content;
   filter.firstChild.addEventListener(`click`, () => {
@@ -12,11 +12,37 @@ const renderFilter = ({textFilter, condition = ``}, onClick = () => {}) => {
 };
 
 class Filter extends Component {
-  constructor() {
+  constructor(data) {
     super();
+    this._name = data.textFilter;
+
+    this._state.isChecked = data.condition;
+
+    this._onFilter = null;
   }
 
+  set onFilter(fn) {
+    this._onFilter = fn.bind(this);
+  }
 
+  _onClickFilter() {
+    if (typeof this._onFilter === `function`) {
+      this._onFilter();
+    }
+  }
+
+  get template() {
+    return `<input type="radio" id="filter-${this._name.toLowerCase()}" name="filter" value="${this._name.toLowerCase()}" ${this._state.isChecked ? `checked` : ``}>
+    <label class="trip-filter__item" for="filter-${this._name.toLowerCase()}">${this._name}</label>`;
+  }
+
+  createListeners() {
+    this._element.addEventListener(`click`, this._onClickFilter);
+  }
+
+  removeListeners() {
+    this._element.removeEventListener(`click`, this._onClickFilter);
+  }
 }
 
 export default (filtersData, onClick) => {
