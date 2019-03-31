d279736924fa2fa8ca82f31bbb3540c92bfb08a1
DmitriyS87 <schukindmitriy@gmail.com>
2019-03-31 14:49:56 +0800

    adds base functions to Model

---
+++ src/model.js
@@ -0,0 +1,56 @@
+class Model {
+  set points(array) {
+    this._points = array;
+    console.log(this._points);
+  }
+  get points() {
+    if (this._points instanceof Array) {
+      return this._points;
+    }
+    return [];
+  }
+
+  set destinations(array) {
+    this._destinations = array;
+  }
+
+  get destinationsNames() {
+    return this._destinations.map((it) => {
+      return it.name;
+    });
+  }
+
+
+  set currentDestination(name) {
+    this._currentDestination = name;
+  }
+
+
+  get currentDestinationData() {
+    if (this._destinations instanceof Array && this._currentDestination) {
+      return this._destinations.filter((it) => {
+        return it.name === this._currentDestination.name;
+      });
+    }
+    return [];
+  }
+
+  set offers(array) {
+    this._offers = array;
+  }
+
+  set currentType(type) {
+    this._currentType = type;
+  }
+
+  get currentTypeOffers() {
+    if (this._offers instanceof Array && this._currentType) {
+      return this._offers.filter((it) => {
+        return it.type === this._currentType;
+      }).offers;
+    }
+    return [];
+  }
+}
+
+export {Model};
